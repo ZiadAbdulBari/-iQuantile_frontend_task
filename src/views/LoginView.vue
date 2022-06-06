7d232afe7786181eac4cbc77ff3b57a98ee93c6e
Ziad Abdul Bari <ziadabdulbari01@gmail.com>
2022-06-06 22:39:14 +0600

    login page design

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,26 +2,28 @@
 <div>
     <Header/>
     <div class="container mx-auto">
-        <div class="wrapper bg-gray-100 p-8 absolute">
-            <div class="alert alert-danger" role="alert" v-if="isUserDataFound===false">
-                {{mgs}}
-            </div>
-            <div class="login-form">
-                <form @submit="login">
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="">Email address</label>
-                        <input v-model="email" type="email" class="mt-3 w-full rounded-md border focus:outline-none border-solid border-slate-300 h-10 p-5" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="" v-if="emailError!=''">{{emailError}}</div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="">Password</label>
-                        <input v-model="password" type="password" class="mt-3 w-full rounded-md border focus:outline-none border-solid border-slate-300 h-10 p-5" id="exampleInputPassword1">
-                        <div id="emailHelp" class="" v-if="passwordError!=''">{{passwordError}}</div>
-                    </div>
-                    <div class="mt-8 text-center">
-                        <button type="submit" @click.prevent="login()" class="bg-[#D61C4E] px-8 py-1 rounded text-white">Submit</button>
-                    </div>
-                </form>
+        <div class="flex justify-center items-center h-screen">
+            <div class="wrapper bg-gray-100 p-8">
+                <div class="alert alert-danger" role="alert" v-if="isUserDataFound===false">
+                    {{mgs}}
+                </div>
+                <div class="login-form">
+                    <form @submit="login">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="">Email address</label>
+                            <input v-model="email" type="email" class="mt-3 w-full rounded-md border focus:outline-none border-solid border-slate-300 h-10 p-5" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <div id="emailHelp" class="" v-if="emailError!=''">{{emailError}}</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="">Password</label>
+                            <input v-model="password" type="password" class="mt-3 w-full rounded-md border focus:outline-none border-solid border-slate-300 h-10 p-5" id="exampleInputPassword1">
+                            <div id="emailHelp" class="" v-if="passwordError!=''">{{passwordError}}</div>
+                        </div>
+                        <div class="mt-8 text-center">
+                            <button type="submit" @click.prevent="login()" class="bg-[#D61C4E] px-8 py-1 rounded text-white">Submit</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
