569487f293d66bbd147d0679414592707423075a
ZiadAbdulBari <ziadabdulbari01@gmail.com>
2022-06-09 15:50:55 +0600

    cart

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,42 +1,49 @@
 <template>
 <div>
     <Header/>
-    <div class="container mx-auto">
-        <div class="cart-product mt-5">
-            <div v-for="(data,i) in cartData" :key="i">
-                <div class="flex items-center justify-between rounded w-500 bg-gray-50 px-5 py-5 mb-5">
-                    <div class="cart-thumpnale">
-                        <img :src="data.photoUrl" alt="" class="rounded">
-                    </div>
-                    <div class="product-name">
-                        <h5>{{data.title}}</h5>
-                    </div>
-                    <div class="flex quantity">
-                        <div>
-                            <button @click.prevent="changeQuantity('increment',data.productId,data.quentity)">
-                                +
-                            </button>
+    <div class="container mx-auto relative">
+        <div class="flex gap-5 w-full">
+            <div class="cart-product mt-5 w-[70%]">
+                <div v-for="(data,i) in cartData" :key="i">
+                    <div class="flex items-center justify-between rounded w-500 bg-gray-50 px-5 py-5 mb-5">
+                        <div class="cart-thumpnale">
+                            <img :src="data.photoUrl" alt="" class="rounded">
                         </div>
-                        <div>
-                            <input type="text" :value="data.quentity" style="width:40px;text-align:center" class="rounded-md border focus:outline-none border-solid border-slate-300 mx-3" readonly>
+                        <div class="product-name">
+                            <h5>{{data.title}}</h5>
                         </div>
-                        <div>
-                            <button @click.prevent="changeQuantity('decrement',data.productId,data.quentity)">
-                                -
-                            </button>
+                        <div class="flex quantity">
+                            <div>
+                                <button @click.prevent="changeQuantity('increment',data.productId,data.quentity)">
+                                    +
+                                </button>
+                            </div>
+                            <div>
+                                <input type="text" :value="data.quentity" style="width:40px;text-align:center" class="rounded-md border focus:outline-none border-solid border-slate-300 mx-3" readonly>
+                            </div>
+                            <div>
+                                <button @click.prevent="changeQuantity('decrement',data.productId,data.quentity)">
+                                    -
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="price">
-                        <h5>{{data.totalPrice}}BDT</h5>
-                    </div>
-                    <div @click.prevent="deleteFromCart(data.productId)">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-                        </svg>
+                        <div class="price">
+                            <h5>{{data.totalPrice}}BDT</h5>
+                        </div>
+                        <div @click.prevent="deleteFromCart(data.productId)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                            </svg>
+                        </div>
                     </div>
                 </div>
+                
+            </div>
+            <div class="total-price bg-[#D61C4E] w-[30%] fixed top-0 right-0">
+                hello
             </div>
         </div>
+        
     </div>
     <div class="toast-wrapper" v-if="mgs!=''">
         <div class="toast-mgs" :class="{ 'success': type=='success','warning': type=='info'}">
@@ -49,7 +56,7 @@
 <script>
     import Header from '../components/Header.vue';
     import {onMounted, ref,reactive,toRefs} from 'vue';
-import { useRouter } from 'vue-router';
+    import { useRouter } from 'vue-router';
     export default {
         components: { Header },
         setup(){
@@ -122,9 +129,13 @@ import { useRouter } from 'vue-router';
 </script>
 
 <style>
-    .cart-product{
-        max-width: 800px;
+    .total-price{
+        height: 60vh;
+
     }
+    /* .cart-product{
+        width: 1100px;
+    } */
     .cart-thumpnale{
         width: 100px;
     }
