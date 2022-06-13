5249dfa6dd909c24e2dc01434179f947e33e80dd
ZiadAbdulBari <ziadabdulbari01@gmail.com>
2022-06-13 16:44:20 +0600

    card correction

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <div class="all-product">
+        <div class="all-product flex flex-wrap justify-start">
             <div v-for="(product,i) in products" :key="i" class="bg-zinc-100 rounded-md overflow-hidden">
                 <div class="card" style="width: 18rem;">
                 <img class="card-img-top" :src="product.photoUrl" alt="Card image cap">
                 <div class="card-body px-3 py-6 leading-9">
                     <p class="font-medium text-lg">{{product.title}}</p>
                     <p class="card-text">Category: {{product.category}}</p>
-                    <p class="font-medium">Price: {{product.price}}BDT</p>
+                    <p class="font-medium">Price: {{product.price}} BDT</p>
                     <div class="flex">
                     <button @click.prevent="addToCart(product)" class="px-3 py-0 bg-[#D61C4E] rounded text-white mt-5">Add to cart</button>
                     <!-- <button @click.prevent="addToCart(product)" class="px-3 py-0 bg-[#D61C4E] rounded text-white mt-5">Add to cart</button> -->
@@ -64,11 +64,11 @@
 
 <style>
 .all-product{
-  display: flex;
+  /* display: flex;
   row-gap:20px;
-  column-gap:20px;
+  column-gap:20px; */
   margin-top: 20px;
-  flex-wrap:wrap;
+  /* flex-wrap:wrap; */
 }
 .card-img-top{
   width:286px;
